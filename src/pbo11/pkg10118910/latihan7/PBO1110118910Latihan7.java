fe58832997032cbd7865cef6a3ee0dd4bbfa0813
ADMIN <ADMIN@ADMIN-PC>
2019-09-20 23:23:55 +0700

    Latihan-Java

---
+++ src/pbo11/pkg10118910/latihan7/PBO1110118910Latihan7.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo11.pkg10118910.latihan7;
+
+/**
+ *
+ * @author
+ * NAMA  : Muhamad Syifa Amrulloh 
+ * KELAS : PBO11K 
+ * NIM   : 10118910
+ *
+ * Description Program : Program ini Berisi pengunaaan Type Data Integer
+ */
+public class PBO1110118910Latihan7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000l;
+        System.out.println("byte : " + b);
+        System.out.println("short: " + s);
+        System.out.println("int  : " + i);
+        System.out.println("long : " + l);
+    }
+    
+}
